7091c1e2b42f8fa340c5988177ed62597f0adae1
Christian <alvescastro00cca13@gmail.com>
2020-09-28 12:19:47 -0300

    Aula 03

--- Modulo-01/aula3/index.js
+++ Modulo-01/aula3/index.js
@@ -4,20 +4,14 @@
  2 Obter o endereco do usuario pelo Id
  */
 
-function obterUsuario() {
-    // quando der algum problema  -> reject(ERRO)
-    // quando der tudo certo -> resolve
-    return new Promise(function resolvePromise(resolve, reject) {
-
+function obterUsuario(callback) {
         setTimeout(() => {
-            return resolve({
+            return callback({
                 id: 1,
                 nome: "Aladin",
                 dataNascimento: new Date()
             })
         }, 1000);
-
-    })
 
 }
 
@@ -40,51 +34,41 @@ function obterEndereco(idUsuario, callback) {
 }
 
 
-const usuarioPromise = obterUsuario()
-//para manipular o sucesso, usamos a função .then()
-//para manipular os erros, usamos o .catch
-usuarioPromise
-    .then(function (resultado) {
-        console.log("resultado", resultado)
+
+
+
+function resolverUsuario(erro, usuario) {
+    console.log('usuario', usuario)
+}
+
+obterUsuario(function resolverUsuario(error, usuario) {
+    //null || 0 || 0 === false
+    if (error) {
+        console.error("Deu ruim em Usuario", error)
+        return
+    }
+
+    obterTelefone(usuario.id, function resolverTelefone(error1, telefone) {
+        if (error1) {
+            console.error("Deu ruim em Telefone", error1)
+            return
+        }
+
+        obterEndereco(usuario.id, function resolverEndereco(error2, endereco) {
+            if (error2) {
+                console.error("Deu ruim no Endereço", error2)
+            }
+
+            console.log(`
+                Nome: ${usuario.nome},
+                Endereço: ${endereco.rua}, ${endereco.numero}
+                Telefone: (${telefone.ddd})${telefone.telefone} 
+
+            `)
+
+        })
     })
-    .catch(function(error){
-        console.error("DEU RUIM", error)
-    })
-
-
-
-// function resolverUsuario(erro, usuario) {
-//     console.log('usuario', usuario)
-// }
-
-// obterUsuario(function resolverUsuario(error, usuario) {
-//     //null || 0 || 0 === false
-//     if (error) {
-//         console.error("Deu ruim em Usuario", error)
-//         return
-//     }
-
-//     obterTelefone(usuario.id, function resolverTelefone(error1, telefone) {
-//         if (error1) {
-//             console.error("Deu ruim em Telefone", error1)
-//             return
-//         }
-
-//         obterEndereco(usuario.id, function resolverEndereco(error2, endereco) {
-//             if (error2) {
-//                 console.error("Deu ruim no Endereço", error2)
-//             }
-
-//             console.log(`
-//                 Nome: ${usuario.nome},
-//                 Endereço: ${endereco.rua}, ${endereco.numero}
-//                 Telefone: (${telefone.ddd})${telefone.telefone} 
-
-//             `)
-
-//         })
-//     })
-// })
+})
 
 // const telefone = obterTelefone(usuario.id)
 
