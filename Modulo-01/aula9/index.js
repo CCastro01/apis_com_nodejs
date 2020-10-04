e8ce44e26c317c00f07c513eb9676680d242c41b
Christian <alvescastro00cca13@gmail.com>
2020-10-04 11:13:14 -0300

    EventEmitter

---
+++ Modulo-01/aula9/index.js
@@ -0,0 +1,16 @@
+const EventEmitter = require('events')
+
+//criando nossa própria classe que extende todos os métodos da EventEmitter 
+class MeuEmissor extends EventEmitter{
+
+}
+
+const meuEmissor = new MeuEmissor() //instanciando uma classe
+const nomeEvento =  'usuario:click' //criando um manipulador de eventos
+
+meuEmissor.on(nomeEvento, function(click){
+    console.log('um usuario clicou', click)
+})
+
+meuEmissor.emit(nomeEvento,'na barra de rolagem')
+meuEmissor.emit(nomeEvento,'no ok')
