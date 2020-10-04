3b4e3d30bc8710477da3868b45ff018d12b38516
Christian <alvescastro00cca13@gmail.com>
2020-10-04 11:29:47 -0300

    EventEmitter e Promise

--- Modulo-01/aula9/index.js
+++ Modulo-01/aula9/index.js
@@ -22,6 +22,15 @@ meuEmissor.on(nomeEvento, function(click){
 
 
 const stdin = process.openStdin()
-stdin.addListener('data', function(value){
-    console.log(`Você digitou: ${value.toString().trim()}`)
+function main(){
+    return new Promise(function(resolve, reject){
+        stdin.addListener('data', function(value){
+            // console.log(`Você digitou: ${value.toString().trim()}`)
+            return resolve(value)
+        })
+
+    })
+}
+main().then(function(resultado){
+    console.log('resultado', resultado.toString())
 })
