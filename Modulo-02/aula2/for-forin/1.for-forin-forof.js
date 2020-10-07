285ef856a1aafbddffd5b14b6f08ecc821babe1f
Christian <alvescastro00cca13@gmail.com>
2020-10-07 12:27:10 -0300

    for in

--- Modulo-02/aula2/for-forin/1.for-forin-forof.js
+++ Modulo-02/aula2/for-forin/1.for-forin-forof.js
@@ -6,13 +6,25 @@ async function main(){
         const result = await service.obterPessoas('a')
         const names = []
 
+        // console.time('for')
+        // for(let i = 0; i<=result.results.length -1; i++){
+        //     const pessoa = result.results[i]
+        //     names.push(pessoa.name)
+        // }
+        // console.timeEnd('for')
+
+
+
         console.time('for')
-        for(let i = 0; i<=result.results.length -1; i++){
+        for(let i in result.results){
             const pessoa = result.results[i]
             names.push(pessoa.name)
         }
         console.timeEnd('for')
+
+
         console.log(`names`, names)
+
         
     }catch(error){
         console.error(`erro interno`, error)
