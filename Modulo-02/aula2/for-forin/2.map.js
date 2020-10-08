09b86db86f49e6e960e532172c75695599328558
Christian <alvescastro00cca13@gmail.com>
2020-10-08 16:01:31 -0300

    map

--- Modulo-02/aula2/for-forin/2.map.js
+++ Modulo-02/aula2/for-forin/2.map.js
@@ -5,11 +5,21 @@ async function main(){
         const results = await service.obterPessoas(`a`)
         const names = []
 
-        results.results.forEach(function(item){
-            names.push(item.name)
-        })
+        // results.results.forEach(function(item){
+        //     names.push(item.name)
+        // })
+        // console.log('names', names)
 
-        console.log('names', names)
+
+        // const names2 = results.results.map(function (pessoa){
+        //     return pessoa.name
+        // })
+        // console.log('names2', names2)
+
+
+        const names3 = results.results.map(pessoa => pessoa.name)
+        console.log('names3', names3)
+
     } catch (error) {
         console.log(`DEU RUIM`, error)
     }
